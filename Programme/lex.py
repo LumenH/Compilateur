204def9de11ea9f0cb906ed7062f4406b3b7b880
LumenH <aurelie.debrot@he-arc.ch>
2016-12-13 15:20:42 +0100

    fix merge conflict

--- Programme/lex.py
+++ Programme/lex.py
@@ -28,13 +28,12 @@ tokens = (
     'IDENTIFIER',
     'ADD_OP',
     'MUL_OP',
-    'DIV_OP',
     'EGAL',
-    'OPEN_LT'
-    'CLOSE_GT',
+    'OPEN_LT'#<
+    'CLOSE_GT',#>
     'APOSTROPHE',
     'GUILLEMET'
-)
+)+tuple(map(lambda s:s.upper(), reserved_words))
 
 
 def t_CLOSE_GT(t):
@@ -53,3 +52,62 @@ def t_IDENTIFIER(t):
 def t_OPEN_LT(t):
     r'<'
     return t
+
+
+def t_NUMBER(t):
+    r'\d+(\.\d+)?'
+    try:
+        t.value=float(t.value)
+    except ValueError:
+        print("Line %d: Problem while parsing %s !"%(t.lineno,t.value))
+        t.value = 0
+    return t
+
+
+def t_ADD_OP(t):
+    r'[+-]'
+    return t
+
+
+def t_MUL_OP(t):
+    r'[*/]'
+    return t
+
+
+def t_EGAL(t):
+    r'[=]'
+    return t
+
+
+def t_APOSTROPHE(t):
+    r'\'[A-Za-z"]+\''
+    return t
+
+
+def t_GUILLEMET(t):
+    r'\"[A-Za-z]+\"'
+    return t
+
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+t_ignore = ' \t'
+
+def t_error(t):
+    print("Illegal character '%s'"%repr(t.value[0]))
+    t.lexer.skip(1)
+
+lex.lex()
+
+if __name__=="__main__":
+    import sys
+    prog = open(sys.argv[1].read())
+
+    lex.input(prog)
+
+    while 1:
+        tok = lex.token()
+        if not tok: break
+        print("line %d: %s(%s)"%(tok.lineno, tok.type, tok.value))
